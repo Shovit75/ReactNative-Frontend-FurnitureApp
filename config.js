53a62c96ba79a779bdc9b8e9a8c34a01d8e6ccb2
Shovit <121250316+Shovit75@users.noreply.github.com>
2024-08-26 01:00:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const BACKEND_URL = 'http://10.0.2.2:8000/api';
 export const STORAGE_URL = 'http://10.0.2.2:8000/storage';
 
-// export const BACKEND_URL = 'http://192.168.1.2:8000/api';
-// export const STORAGE_URL = 'http://192.168.1.2:8000/storage';
+// export const BACKEND_URL = 'Your Backend API';
+// export const STORAGE_URL = 'Your Backend API Storage';
